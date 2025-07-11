de28ae3d9a7a839266a53e674e01bccb8999472c
João Balieiro <silva.balieiro.joao@gmail.com>
2025-07-11 17:06:27 -0300

    Update 4_enviar_curriculos.py

--- pages/4_enviar_curriculos.py
+++ pages/4_enviar_curriculos.py
@@ -32,7 +32,7 @@ import psutil
 
 from langchain_community.callbacks.manager import get_openai_callback
 
-artifacts_dir = Path(__file__).parent.parent / "docling_models" / "ds4sd--CodeFormula"
+artifacts_dir = Path(__file__).parent.parent / "docling_models" #/ "ds4sd--CodeFormula"
 
 pipeline_options = PdfPipelineOptions(
     artifacts_path = str(artifacts_dir),
